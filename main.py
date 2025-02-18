10de6edbded3348fe95e05a6e4610c58e5e1540c
Sergei Klokov <s2584835@ya.ru>
2025-02-18 19:09:39 +0300

    main keyboards/main_menu: добавил в main запрос на установку главного меню в бота

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram.enums import ParseMode
 
 from config_data.config import Config, load_config
 from handlers import user_handlers, other_handlers
+from keyboards.main_menu import set_main_menu
 
 
 logger = logging.getLogger(__name__)
@@ -26,6 +27,8 @@ async def main():
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
+
+    await set_main_menu(bot)
 
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
