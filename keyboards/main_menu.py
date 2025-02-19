74f08bd23d39d57dd34578b4062c9911cddcb062
Sergei Klokov <s2584835@ya.ru>
2025-02-19 17:16:53 +0300

    keyboards/main_menu.py - реализовал загрузку главного меню в бота

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,5 +1,12 @@
 from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lexicon.lexicon import LEXICON_COMMANDS
 
 
 async def set_main_menu(bot: Bot):
-    pass
+    main_menu_command = [BotCommand(
+        command=command,
+        description=description
+    ) for command, description in LEXICON_COMMANDS.items()]
+    await bot.set_my_commands(main_menu_command)
