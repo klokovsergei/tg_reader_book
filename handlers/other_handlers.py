60f73530ccd52574b042e8b48e73760143669395
Sergei Klokov <s2584835@ya.ru>
2025-02-19 18:43:04 +0300

    скорректировал по pep8

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,4 +1,11 @@
 from aiogram import Router
+from aiogram.types import Message
 
+from lexicon.lexicon import LEXICON
 
 router = Router()
+
+
+@router.message()
+async def send_unknown_message(message: Message):
+    await message.answer(LEXICON['unknown_message'])
